d6809175d3d140e0f93579b6593fe6906be377d9
Matej Novotny <manovotn@redhat.com>
2017-01-16 12:29:23 +0100

    Added all Weld modules to simulate even more clashes. Should be fixed in Weld CR1.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,4 +5,8 @@ module org.none.jdk9playground {
     requires weld.core.impl;
     requires weld.environment.common;
     requires weld.api;
+    requires weld.web;
+    requires weld.jsf;
+    requires weld.jta;
+    requires weld.ejb;
 }
